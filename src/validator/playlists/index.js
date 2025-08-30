65411cdb9ddbadbff1105ea0b631241846962401
Irfan Dwi Samudra <irfandwisamudra08@gmail.com>
2025-08-30 17:29:10 +0700

    feat(validator): add playlists validation schemas and functions

---
+++ src/validator/playlists/index.js
@@ -0,0 +1,22 @@
+const {
+  PlaylistPayloadSchema,
+  SongToPlaylistPayloadSchema,
+} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const PlaylistsValidator = {
+  validatePlaylistPayload: (payload) => {
+    const validationResult = PlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateSongToPlaylistPayload: (payload) => {
+    const validationResult = SongToPlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsValidator;
