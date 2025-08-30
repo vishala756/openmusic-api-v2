1a6bda37f4fddb8104e4e52c7f68fe6cc0521a00
Irfan Dwi Samudra <irfandwisamudra08@gmail.com>
2025-08-30 18:53:52 +0700

    feat(utils): update mapDBToModel to include playlistId, songId, and userId

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,17 @@
 /* eslint-disable camelcase */
 
-const mapDBToModel = ({ album_id, ...args }) => ({
+const mapDBToModel = ({
+  album_id,
+  playlist_id,
+  song_id,
+  user_id,
+  ...args
+}) => ({
   ...args,
   albumId: album_id,
+  playlistId: playlist_id,
+  songId: song_id,
+  userId: user_id,
 });
 
 module.exports = { mapDBToModel };
